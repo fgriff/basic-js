24e078f847c2fb868ae23a17a14e3fdf5c5ad99c
fgriff <>
2021-03-06 20:57:43 +0300

    feat: implement extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  if (!options) return str;
+
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  const addition = options.hasOwnProperty('addition') ? String(options.addition) : '';
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+
+  str = String(str);
+
+  let add = addition + additionSeparator;
+
+  add = add.repeat(additionRepeatTimes);
+
+  add = add.slice(0, -additionSeparator.length);
+
+  str += add + separator;
+
+  str = str.repeat(repeatTimes);
+
+  return str.slice(0, -separator.length);
+}
+
+
+
   
