29358ed5bf7523b2cebc075f8ae19576c94aa747
fgriff <>
2021-03-06 17:28:06 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('Argument is not Array');
+
+  const res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        if (i < arr.length) {
+          i += 1;
+          res.push(undefined);
+        }
+        break;
+
+      case '--discard-prev':
+        if (i > 0) res.pop();
+        break;
+
+      case '--double-next':
+        if (i < arr.length - 1) {
+          res.push(arr[i + 1]);
+        }
+        break;
+
+      case '--double-prev':
+        if (i > 0) {
+          res.push(res[res.length - 1]);
+        }
+        break;
+    
+      default:
+        res.push(arr[i]);
+        break;
+    }
+  }
+
+  return res.filter(item => item !== undefined);
+}
