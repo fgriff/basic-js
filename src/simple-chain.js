be4081746f2245fa5cb73a7451d60adf62942c46
fgriff <>
2021-03-06 18:49:59 +0300

    feat: implement simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.chain.push('()');
+      return this;
+    } 
+
+    this.chain.push(value);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position >= this.chain.length ||
+        position <= 0 ||
+        isNaN(position)) {
+      this.chain = [];
+      throw new Error();
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+  
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    for (let i = 0; i < this.chain.length; i++) {
+      this.chain[i] = '( ' + this.chain[i] + ' )~~';
+    }
+
+    let res = this.chain.join('').slice(0, -2);
+    this.chain = [];
+    
+    return res;
   }
+  
 };
 
 module.exports = chainMaker;
