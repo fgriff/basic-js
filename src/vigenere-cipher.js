12bc35a48294477d025cb9f8b2bb97ffe6401d06
fgriff <>
2021-03-06 21:00:36 +0300

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,69 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.direction = direction;
+  }
+
+encrypt(message, key) {
+    if (!message || !key) throw new Error();
+    const arrStr = message.toUpperCase().split('');
+  
+    message = message.toUpperCase().split(' ').join('');
+  
+    const countRepeat = Math.ceil(message.length / key.length);
+  
+    key = key.repeat(countRepeat).slice(0, message.length).toUpperCase();
+  
+    let cipher = '';
+  
+    for (let i = 0; i < arrStr.length; i++) {
+      if (arrStr[i] === ' ') {
+        cipher += arrStr[i];
+        arrStr.splice(i, 1);
+      } 
+  
+      if (arrStr[i].charCodeAt() < 'A'.charCodeAt() || arrStr[i].charCodeAt() > 'Z'.charCodeAt()) {
+        cipher += arrStr[i];
+      } else {
+        const digit = ((arrStr[i].charCodeAt() + key[i].charCodeAt()) % 26) + 65;
+  
+        cipher += String.fromCharCode(digit);
+      }
+    }
+  
+    return this.direction ? cipher : cipher.split('').reverse().join('') ;
+  }
+  
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error();
+    
+    const arrStr = encryptedMessage.toUpperCase().split('');
+
+    encryptedMessage = encryptedMessage.toUpperCase().split(' ').join('');
+
+    const countRepeat = Math.ceil(encryptedMessage.length / key.length);
+
+    key = key.repeat(countRepeat).slice(0, encryptedMessage.length).toUpperCase();
+
+    let cipher = '';
+
+    for (let i = 0; i < arrStr.length; i++) {
+      if (arrStr[i] === ' ') {
+        cipher += arrStr[i];
+        arrStr.splice(i, 1);
+      } 
+
+      if (arrStr[i].charCodeAt() < 'A'.charCodeAt() || arrStr[i].charCodeAt() > 'Z'.charCodeAt()) {
+        cipher += arrStr[i];
+      } else {
+        const digit = ((arrStr[i].charCodeAt() + 26 - key[i].charCodeAt()) % 26) + 65;
+
+        cipher += String.fromCharCode(digit);
+      }
+    }
+
+    return this.direction ? cipher : cipher.split('').reverse().join('') ;
   }
 }
 
