e8d070d9d3929d5e7220ec852c16028cf7bdbb39
fgriff <anglirum@gmail.com>
2025-01-01 22:49:54 +0300

    feat: solve mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    result.push([]);
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      let minesCount = 0;
+
+      const coords = {
+        right: [i, j + 1],
+        rightDown: [i + 1, j + 1],
+        down: [i + 1, j],
+        leftDown: [i + 1, j - 1],
+        left: [i, j - 1],
+        leftUp: [i - 1, j - 1],
+        up: [i - 1, j],
+        rightUp: [i - 1, j + 1],
+      };
+
+      const coordsKeyArr = Object.keys(coords);
+
+      for (let k = 0; k < coordsKeyArr.length; k++) {
+        if (coords[coordsKeyArr[k]][0] >= 0 &&
+            coords[coordsKeyArr[k]][1] >= 0 &&
+            coords[coordsKeyArr[k]][0] < matrix.length &&
+            coords[coordsKeyArr[k]][1] < matrix[i].length) {
+          if (matrix[coords[coordsKeyArr[k]][0]][coords[coordsKeyArr[k]][1]]) {
+            minesCount++;
+          }
+        }
+      }
+
+      result[i].push(minesCount);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
