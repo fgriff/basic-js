e9eb3258484ae163bc037711cd2c43853ab48bc2
fgriff <anglirum@gmail.com>
2025-01-05 13:40:29 +0300

    feat: solve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,77 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirectionReversed = true) {
+    this.isDirectionReversed = isDirectionReversed;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  isDataValid(message, keyWord) {
+    if (!message || !keyWord) {
+      throw new Error('Incorrect arguments!');
+    }
+  }
+
+  transformData(msg, key) {
+    const message = msg.toUpperCase();
+    let keyWord = key.toUpperCase();
+
+    const clearedMessageLength = message.replaceAll(' ', '').length;
+    const repetitionsCount = Math.ceil(clearedMessageLength / keyWord.length);
+    keyWord = keyWord.repeat(repetitionsCount).slice(0, clearedMessageLength);
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === ' ') {
+        keyWord = `${keyWord.slice(0, i)} ${keyWord.slice(i)}`;
+      }
+    }
+
+    return [message, keyWord];
+  }
+
+  isAlphabetSymbol(char) {
+    const startCode = 'A'.codePointAt();
+    const endCode = 'Z'.codePointAt();
+    const charCode = char.codePointAt();
+
+    return startCode <= charCode && charCode <= endCode;
+  }
+
+  run(msg, key, method = 'encrypt') {
+    this.isDataValid(msg, key);
+
+    const [message, keyWord] = this.transformData(msg, key);
+    let cipher = '';
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === ' ') {
+        cipher += message[i];
+        continue;
+      }
+
+      if (this.isAlphabetSymbol(message[i])) {
+        let charCode = null;
+
+        if (method === 'encrypt') {
+          charCode = (message[i].codePointAt() + keyWord[i].codePointAt()) % 26 + 65;
+        } else if (method === 'decrypt') {
+          charCode = (message[i].codePointAt() + 26 - keyWord[i].codePointAt()) % 26 + 65;
+        }
+
+        cipher += String.fromCodePoint(charCode);
+      } else {
+        cipher += message[i];
+      }
+    }
+
+    return this.isDirectionReversed ? cipher : cipher.split('').reverse().join('');
+  }
+
+  encrypt(message, keyWord) {
+    return this.run(message, keyWord);
+  }
+
+  decrypt(encryptedMessage, keyWord) {
+    return this.run(encryptedMessage, keyWord, 'decrypt');
   }
 }
 
